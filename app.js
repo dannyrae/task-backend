642362d270407e79f1f44d701f8373199a6c776e
Emmanuel Asuquo <emmanueldan0920@gmail.com>
2025-04-08 23:34:52 +0100

    update cors

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(cors());
+app.use(
+  cors({
+      origin: "*",
+      credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
