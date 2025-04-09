32c6b63c86be1a6ab0a7f45c2b4307d97662708b
Emmanuel Asuquo <emmanueldan0920@gmail.com>
2025-04-09 22:09:54 +0100

    update: cors origin

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
